704514e23df4fd6459cea4c7511be082967091fd
Riitta Pessi <riitta@pessi.fi>
2025-03-14 13:56:15 +0200

    Ruby harjoittelutehtävä 4

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,12 @@
-# kirjoita koodi tänne
+# Tee metodi tulosta, joka saa parametrina taulukon ja tulostaa sen jokaisen alkion. Kaikki alkiot tulostetaan samalle riville.
+
+def tulosta(taulukko)
+  tulostus = ""
+  taulukko.count.times do |i|
+    tulostus = tulostus + "#{taulukko[i]}"
+  end
+  puts tulostus
+end
+
+tulosta([1, 2])
+tulosta([4, 6, 3, 1, 8])
